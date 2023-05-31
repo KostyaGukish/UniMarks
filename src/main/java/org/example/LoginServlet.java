7d6f37f9f9aff228088ea25ab0cd9e7fc41b3ff6
kandr <kandrat.71@mail.ru>
2023-06-01 02:30:48 +0300

    nice

--- src/main/java/org/example/LoginServlet.java
+++ src/main/java/org/example/LoginServlet.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.dao.PUserDao;
+import org.example.entity.puser;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +17,41 @@ public class LoginServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         // Перенаправление на страницу login.jsp
         request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+        String userlogin = request.getParameter("name");
+        String password = request.getParameter("password");
+        PUserDao pUserDao = new PUserDao();
+        try {
+            puser tryPuser =  pUserDao.findEntityById(userlogin);
+            if(tryPuser.getPassword().equals(password)){
+                request.getRequestDispatcher("/Page1.jsp").forward(request, response);
+                // request.setAttribute("res", delta);
+            }
+            else {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Неверный логин или пароль");
+            }
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Неверный логин или пароль");
+        }
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String userlogin = request.getParameter("name");
+        String password = request.getParameter("password");
+        PUserDao pUserDao = new PUserDao();
+        try {
+            puser tryPuser =  pUserDao.findEntityById(userlogin);
+            if(tryPuser.getPassword().equals(password)){
+                request.getRequestDispatcher("/Page1.jsp").forward(request, response);
+                // request.setAttribute("res", delta);
+            }
+            else {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Неверный логин или пароль");
+            }
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Неверный логин или пароль");
+        }
 
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,8 +1,8 @@
-<%@page isErrorPage="true" language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <title>Вход в учётную запись</title>
     <link rel="stylesheet" href="styles/style.css">
 </head>
@@ -17,35 +17,19 @@
 </style>
 <body>
 
-<form class="box" action="Page1.jsp" method="post">
+<form class="box"  method="post">
     <h1>Login</h1>
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
     <input type="submit" name="login" value="Login">
 </form>
 
+<%-- Проверка наличия ошибки и отображение сообщения об ошибке --%>
+<% if (request.getAttribute("javax.servlet.error.message") != null) { %>
 <script>
-    // Обработка отправки формы
-    document.querySelector('.box').addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        // Получаем значения полей "username" и "password"
-        var username = document.querySelector('input[name="username"]').value;
-        var password = document.querySelector('input[name="password"]').value;
-
-        // Проверяем логин и пароль
-        if (username === 'Андрей Коноплич' && password === '1111') {
-            // Если логин и пароль верны, сохраняем значение "username" в локальном хранилище
-            localStorage.setItem('username', username);
-
-            // Перенаправляем на страницу Page1.html
-            window.location.href = 'Page1.jsp';
-        } else {
-            // Если логин или пароль неверны, отображаем всплывающее сообщение
-            alert('Неверный логин или пароль');
-        }
-    });
+    alert("<%= request.getAttribute("javax.servlet.error.message") %>");
 </script>
+<% } %>
 
 </body>
 </html>
