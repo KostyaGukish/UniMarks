cfd44f9d96e7716ad86a3cf500aac061a4123f39
Detr <dmt.yugg@gmail.com>
2023-06-01 14:13:54 +0300

    added teachers and evaluations querries

---
+++ src/main/java/org/example/entity/SelectedMarks.java
@@ -0,0 +1,22 @@
+package org.example.entity;
+
+public class SelectedMarks {
+    int mark;
+    String subject_name;
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public String getSubject_name() {
+        return subject_name;
+    }
+
+    public void setSubject_name(String subject_name) {
+        this.subject_name = subject_name;
+    }
+}
